eac6457cf835748e02396e27beb9fc31d96eeb6c
AndreiChuyanov <achuyanov@gmail.com>
2019-09-25 22:26:58 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  ret = BigInt(first) * BigInt(second);
+  return ret.toString();
 }
+
+//'329568934658432659586', '58379426534596'), '19240045408977038918943543720037256');
+//let q=BigInt(329568934658432659586*58379426534596);
+//console.log(q.toString());
